2bf6aeafb4a3b5df9a46f148aad993f435371ebd
Jose Manuel Marquez Camarillo <jmarquezcamarillo@gmail.com>
2023-03-23 19:18:33 -0600

    ajustes

--- src/main/java/com/kiubit/webapi/auth/config/JwtAuthenticationFilter.java
+++ src/main/java/com/kiubit/webapi/auth/config/JwtAuthenticationFilter.java
@@ -1,11 +1,16 @@
 package com.kiubit.webapi.auth.config;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.kiubit.webapi.auth.models.AuthenticationResponse;
+import com.kiubit.webapi.auth.models.CommonResponse;
 import com.kiubit.webapi.auth.service.JwtService;
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -23,12 +28,19 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
     private final JwtService jwtService;
     private final UserDetailsService userDetailsService;
 
+    private final ObjectMapper mapper;
+
     @Override
     protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
         final String jwt;
         final String userEmail;
         if(authHeader == null || !authHeader.startsWith("Bearer ")){
+            /*CommonResponse<AuthenticationResponse> respuesta=new CommonResponse<AuthenticationResponse>(new AuthenticationResponse("Must provide a valid token to continue"),false,"Authentication Failed");
+            response.setStatus(HttpStatus.FORBIDDEN.value());
+            response.setContentType(MediaType.APPLICATION_JSON_VALUE);
+
+            mapper.writeValue(response.getWriter(), respuesta);*/
             filterChain.doFilter(request,response);
             return;
         }
